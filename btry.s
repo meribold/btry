03345bbbee315186187eb6f0f320ac2dbda7cbac
Lukas Waymann <git@meribold.org>
2020-04-19 12:24:07 +0800

    Open "/sys/class/power_supply/BAT0/charge_now"
    Output of `strace ./btry`:
    
        execve("./btry", ["./btry"], 0x7fff6d0897a0 /* 45 vars */) = 0
        open("/sys/class/power_supply/BAT0/charge_now", O_RDONLY) = 3
        exit(0)                                 = ?
        +++ exited with 0 +++

--- btry.s
+++ btry.s
@@ -1,7 +1,15 @@
 .global _start
 
 _start:
+    # open("/sys/class/power_supply/BAT0/charge_now", O_RDONLY)
+    mov     $2, %rax          # system call 2 is open
+    mov     $charge_now, %rdi # address of path to open
+    mov     $0, %rcx          # 0 means read-only
+    syscall
+
     # exit(0)
     mov     $60, %rax   # system call 60 is exit
     xor     %rdi, %rdi  # we want return code 0
     syscall             # invoke operating system to exit
+
+charge_now: .ascii "/sys/class/power_supply/BAT0/charge_now"
