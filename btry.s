ac463107919616b3d1be8d407109b11ac687c563
Lukas Waymann <git@meribold.org>
2020-04-22 23:38:23 +0800

    Output (truncated) watt hours

--- btry.s
+++ btry.s
@@ -1,9 +1,9 @@
 .global _start
 
 _start:
-    # open("/sys/class/power_supply/BAT0/charge_now", O_RDONLY)
+    # open("/sys/class/power_supply/BAT0/energy_now", O_RDONLY)
     mov     $2, %rax           # system call 2 is open
-    mov     $charge_now, %rdi  # address of path to open
+    mov     $energy_now, %rdi  # address of path to open
     mov     $0, %rcx           # 0 means read-only
     syscall
 
@@ -13,17 +13,63 @@ _start:
     sub     $8, %rsp    # "allocate" 8 bytes of stack space
     mov     %rsp, %rsi  # save file contents read from fd on the stack
     mov     $8, %rdx    # read up to 8 bytes
-    syscall
+    syscall             # the number of bytes read go into %rax
 
-    # write(1, charge, 8)
-    mov     $1, %rax    # system call 1 is write
-    mov     $1, %rdi    # file handle 1 is stdout
-    mov     %rsp, %rsi  # address of string to output
-    syscall             # skip `mov $8, %rdx` because %rdx is already 8
+    # convert file contents to an integer (stored in %r8)
+    sub     $1, %rax
+    xor     %r8, %r8
+    xor     %rcx, %rcx
+    xor     %rdx, %rdx
+next_char:
+    imul    $10, %r8
+    mov     (%rsp, %rcx), %dl  # load one character/byte/digit
+    sub     $48, %dl           # convert the character from ASCII
+    add     %rdx, %r8          # add this digit
+    inc     %rcx
+    cmp     %rcx, %rax
+    jne     next_char
+
+    # convert from microwatt to watt hours
+    xor     %edx, %edx
+    mov     %r8, %rax       # dividend
+    mov     $1000000, %r9d  # 4-byte divisor
+    div     %r9d            # div stores the quotient in %eax
+    mov     %rax, %r8       # store the quotient in %r8
+
+    # put the string " Wh\n" on the stack
+    dec     %rsp
+    movb    $10, (%rsp)
+    dec     %rsp
+    movb    $104, (%rsp)
+    dec     %rsp
+    movb    $87, (%rsp)
+    dec     %rsp
+    movb    $32, (%rsp)
+
+    # convert %r8 back to text
+    mov     %r8, %rax
+    mov     $10, %r9d  # 4-byte divisor
+    xor     %r8, %r8   # we record the number of character in %r8
+more:
+    xor     %edx, %edx
+    div     %r9d        # quotient and remainder are stored in %eax and %edx, respectively
+    add     $48, %dl    # convert the remainder to ASCII
+    dec     %rsp
+    mov     %dl, (%rsp)
+    inc     %r8
+    cmp     $0, %eax
+    jne     more
+
+    # write(1, energy, %r8 + 4)
+    mov     $1, %rax      # system call 1 is write
+    mov     $1, %rdi      # file handle 1 is stdout
+    mov     %rsp, %rsi    # address of string to output
+    lea     4(%r8), %rdx  # number of bytes: %r8 plus 4 for " Wh\n"
+    syscall
 
     # exit(0)
     mov     $60, %rax   # system call 60 is exit
     xor     %rdi, %rdi  # we want return code 0
     syscall             # invoke operating system to exit
 
-charge_now: .ascii "/sys/class/power_supply/BAT0/charge_now"
+energy_now: .ascii "/sys/class/power_supply/BAT0/energy_now"
